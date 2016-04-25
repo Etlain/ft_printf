d59e4a2cc084e3962d477423012ca80429de01ea
Mounir MOUHSSINE <mmouhssi@e1r8p14.42.fr>
2016-04-25 22:16:42 +0200

    add_wp fonction

--- sources/ft_type.c
+++ sources/ft_type.c
@@ -6,12 +6,12 @@
 /*   By: mmouhssi <mmouhssi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/03 18:45:53 by mmouhssi          #+#    #+#             */
-/*   Updated: 2016/04/25 00:00:44 by mmouhssi         ###   ########.fr       */
+/*   Updated: 2016/04/25 22:15:56 by mmouhssi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
-
+// appliquer modificateur de longueur puis largeur du champs puis precision puis attribut
 /*
 void	ft_truc()
 {
@@ -32,10 +32,68 @@ void	ft_truc()
 	if (str == 'X')
 		ft_putstr(ft_itoah(va_arg(lst, unsigned int), 2));
 }
-*/
-int	ft_type(va_list lst, char str)
+
+void	ft_truc2()
 {
-	//char *nbr;
+	if (str == 's')
+		ft_putstr(va_arg(lst, char *));
+	else if (str == 'S')
+		ft_putwstr(va_arg(lst, wchar_t *));
+	else if (str == 'p')
+	{
+		ft_putstr("0x"); //ptet ajouter un 10
+		ft_putstr(ft_itoah(va_arg(lst, int), 1));
+	}
+	else if (str == 'c')
+		ft_putchar((char)va_arg(lst, int));
+	else if (str == 'C')
+		ft_putwchar(va_arg(lst, wchar_t));
+}
+*/
+
+char	*add_wp(t_format format, va_list lst, char *type) // reflechir pour wchat_t // penser condition -
+{
+	int width;
+	int i;
+	char *str;
+	char *str2;
+
+	width = 0;
+	str = type;
+	if (format.width == NULL)
+		return (type);
+	else if (format.width[0] == '*')
+		width = va_arg(lst, int) - ft_strlen(type);
+	else if (format.width[0] >= '0' && format.width[0] <= '9')
+		width = ft_atoi(format.width) - ft_strlen(type);
+	else
+		return (type);
+	i = 0;
+	if (format.flags != NULL && format.flags[0] == '0' && width > 0)
+	{
+		while (i < width)
+		{
+			str2 = str;
+			str = ft_strjoin("0", str2);
+			free(str2);
+			i++;
+		}
+		return (str);
+	}
+	else
+	{
+		while (i < width && width > 0)
+		{
+			ft_putchar(' ');
+			i++;
+		}
+	}
+	return (type);
+}
+
+int	ft_type(t_format format, va_list lst, char str)
+{
+	char *type;
 
 	if (str == 's')
 		ft_putstr(va_arg(lst, char *));
@@ -48,10 +106,11 @@ int	ft_type(va_list lst, char str)
 	}
 	else if (str == 'd' || str == 'i')
 	{
-		//nbr = ft_itoa(va_arg(lst, int));
+		type = ft_itoa(va_arg(lst, int));
 		//nbr = ft_strjoin("00", nbr); // pense a free use fonction rempli 0 et strlen
-		//ft_putstr(nbr);
-		ft_putnbr(va_arg(lst, int));
+		type = add_wp(format, lst, type);
+		ft_putstr(type);
+		//ft_putnbr(va_arg(lst, int));
 	}
 	else if (str == 'D')
 		ft_putnbr(va_arg(lst, long int));
@@ -71,7 +130,7 @@ int	ft_type(va_list lst, char str)
 		ft_putchar((char)va_arg(lst, int));
 	else if (str == 'C')
 		ft_putwchar(va_arg(lst, wchar_t));
-	else if (str == 'b')
+	else if (str == 'b')				// bonus pas integrer
 		ft_putstr(ft_itoab(va_arg(lst, unsigned int)));
 	else
 		return (0);
